9a15494abd430ee8ffe24b144f04764f3c3bf775
Pablo de Haro <pablitar@gmail.com>
2014-05-03 13:00:06 -0300

    El pretty print era un fiasco

--- src/console_reporter.rb
+++ src/console_reporter.rb
@@ -6,11 +6,11 @@ class ConsoleReporter
 
   def agregar_fallo(clase, metodo, assertion_error)
     puts "FAIL #{clase.name}>>#{metodo}: #{assertion_error}"
-    puts "#{assertion_error.backtrace.pretty_print}"
+    puts "#{assertion_error.backtrace.join "\n"}"
   end
 
   def agregar_error(clase, metodo, error)
     puts "ERROR #{clase.name}>>#{metodo}: #{error}"
-    puts "#{error.backtrace.pretty_print}"
+    puts "#{error.backtrace.join "\n"}"
   end
 end
